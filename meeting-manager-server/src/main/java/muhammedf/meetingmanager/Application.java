24065d02301786807bcca1a4dfeec3cbdb3fbd62
furkan <mfg263@gmail.com>
2017-09-14 03:45:51 +0300

    delete demoapplication

--- meeting-manager-server/src/main/java/muhammedf/meetingmanager/Application.java
+++ meeting-manager-server/src/main/java/muhammedf/meetingmanager/Application.java
@@ -1,4 +1,4 @@
-package muhammedf;
+package muhammedf.meetingmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 	@Bean
